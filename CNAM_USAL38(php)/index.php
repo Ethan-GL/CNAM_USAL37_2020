63b7edd09975577161a5e8082b7cd2e27b7b667b
EthanG <Ethan-GL@users.noreply.github.com>
2020-12-04 15:46:48 +0100

    modif exo 1

--- CNAM_USAL38(php)/index.php
+++ CNAM_USAL38(php)/index.php
@@ -6,10 +6,12 @@ $age = 20;
 
 $x = $retraite - $age;
 
+$y = $age - $retraite;
+
 
 
 if($age > $retraite) {
-    echo "Vous êtes à la retraite depuis" .$x;
+    echo "Vous êtes à la retraite depuis" .$y;
 }
 elseif($age < $retraite) {
     echo "Il vous reste " .$x ."année avant d'être à la retraite";
