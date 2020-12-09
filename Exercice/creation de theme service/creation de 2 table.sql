b880f973176ce7760f674b042edb3f94f98f850e
EthanG <Ethan-GL@users.noreply.github.com>
2020-12-09 11:01:27 +0100

    modif

--- Exercice/creation de theme service/creation de 2 table.sql
+++ Exercice/creation de theme service/creation de 2 table.sql
@@ -1,21 +1,16 @@
-
-
-USE usal37_agence;
-
-
 CREATE TABLE theme 
 (
-	theme_code CHAR(32) PRIMARY KEY,
-    theme_name VARCHAR(32) NOT NULL,
+    theme_code CHAR(32) PRIMARY KEY,
+    theme_name VARCHAR(32) NOT NULL UNIQUE,
     theme_description VARCHAR(255)
 );
 
 CREATE TABLE service
 (
-	service_code CHAR(32) PRIMARY KEY,
-    service_name VARCHAR(32) NOT NULL,
+    service_code CHAR(32) PRIMARY KEY,
+    service_name VARCHAR(32) NOT NULL UNIQUE,
     service_description CHAR(255) ,
-    service_score CHAR(32)NOT NULL,
+    service_score CHAR(32) NOT NULL,
     service_commentaire VARCHAR(255)
 );
 
