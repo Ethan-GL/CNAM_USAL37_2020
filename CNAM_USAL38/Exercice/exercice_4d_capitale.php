2d7e01d23d4a4bee8b5457419f5e2eabd2b4649a
EthanG <Ethan-GL@users.noreply.github.com>
2020-12-11 16:20:05 +0100

    correction capital

--- CNAM_USAL38/Exercice/exercice_4d_capitale.php
+++ CNAM_USAL38/Exercice/exercice_4d_capitale.php
@@ -13,14 +13,21 @@ $pays = [
 // array_key_exists()
 // empty()
 
-function capitalCity(string $nom_pays)
-{
-    // A vous de jouer !
+function capitalCity(string $nom_pays){
 
-    return "";  // le nom de la capitale
+    global $pays;
+
+    if(array_key_exists($nom_pays, $pays)){
+        return $pays[$nom_pays];
+    }else{
+        return "capital inconnue";
+    }
+    
+    
 }
 
+$capitale = capitalcity('France');
 
-echo capitalCity('France'); // affiche Paris
+echo $capitale ;// affiche Paris
 echo capitalCity('Belgique'); // affiche Bruxelles
 echo capitalCity('Suisse'); // affiche Capitale inconnue
