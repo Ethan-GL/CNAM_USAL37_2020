92083c3afb133a268ee4542b51d1686587751282
EthanG <Ethan-GL@users.noreply.github.com>
2020-12-11 13:53:11 +0100

    modif retraite

--- CNAM_USAL38/Exercice/retraite.php
+++ CNAM_USAL38/Exercice/retraite.php
@@ -1,20 +1,26 @@
 <?php
-
 $retraite = 55;
 
-$age = 20;
+$age = 2;
 
 $x = $retraite - $age;
+
+$y = $age - $retraite;
+
 
 
 
 if($age > $retraite) {
-    echo "Vous êtes à la retraite depuis" .$x;
+    echo "Vous êtes à la retraite depuis " .$y ." ans"; 
 }
 elseif($age < $retraite) {
-    echo "Il vous reste " .$x ."année avant d'être à la retraite";
+    if($age < 18 && $age >0){
+        echo "un peut jeune pour penser à la retraite ";
+    }elseif($age < 0){
+        echo "Vous êtes pas né";
+    }else{
+        echo "Il vous reste " .$x ." année avant d'être à la retraite";}
 }
-else  {
+else{
     echo "La retraite c'est  cette année";
 }
-
